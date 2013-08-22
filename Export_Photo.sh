00ee285e8c2f41885687a437393493d0c363ee56
ben <krilyat@gmail.com>
2013-08-22 19:40:26 +0200

    debut du script d'export

---
+++ Export_Photo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SOURCE=$1
+DEST=$2
+
+sourceFile=$(find $SOURCE -print)
+
+for file in ${sourceFile[*]} ;do
+echo "'$file'"
+done
+
+
+
+
+exit 12
+
+    case $(file -b "$File") in
+    "JPEG*") echo "JPEEEEEEEG";;
+    esac
+date --date $(exiftime -tg -s '//' 100OLYMP/P1010961.JPG | cut -d'/' -f3 | cut -d' ' -f1 | tr ':' '/') +%Y/%B/%d-%A
+
+
