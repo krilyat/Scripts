5d8a7fc1a46367dce0d13612ca78bcee970b6a1a
ben <krilyat@gmail.com>
2013-10-21 17:24:58 +0200

    add verfication of the output file

--- remove_mkv_header.sh
+++ remove_mkv_header.sh
@@ -2,9 +2,13 @@
 FILM=$1
 
 if [ $(mkvinfo $FILM | grep -c header) -ne 0 ] ;then
-	echo "remove compression header from $FILM"
 	OLDFILM="${FILM::${#FILM}-4}.mkv"
 	NEWFILM="${FILM::${#FILM}-4}_remux.mkv"
+    if [ -f $NEWFILM ] ;then
+        echo "nothing to do"
+        exit 42
+    fi 
+	echo "remove compression header from $FILM"
 	echo "Strat processiong..."
 	echo "mkvmerge -o $NEWFILM --engage keep_bitstream_ar_info -A -S --compression -1:none $FILM -D -S --compression -1:none $FILM -A -D --compression -1:none $FILM"
 	#mkvmerge -o $NEWFILM --engage keep_bitstream_ar_info -A -S --compression -1:none $FILM -D -S --compression -1:none $FILM -A -D --compression -1:none $FILM
